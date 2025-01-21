00cdc090c2222da41b108febe85266c36b6f4b7f
KrishnaM023 <krishna050120>
2025-01-21 14:52:37 +0530

    Chat app project completed

---
+++ backend/src/routes/message.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.js";
+
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
